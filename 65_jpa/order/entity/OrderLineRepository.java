6dfc531fc5a103aa7de7ef8fab9676d023aef88d
churogrammer <m_sense1031@naver.com>
2023-04-25 22:40:33 +0900

    mod : 기존 폴더 삭제

--- 65_jpa/order/entity/OrderLineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duniv.blog.domain.order.entity;
-
-import com.duniv.blog.domain.order.entity.OrderLineEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderLineRepository extends JpaRepository<OrderLineEntity, Integer> {
-}
--- 65_jpa/order/entity/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duniv.blog.domain.order.entity;
-
-import com.duniv.blog.domain.order.emb.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
-}
